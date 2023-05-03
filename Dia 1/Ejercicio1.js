2bf1bfda166fedfc8e0d86e4faa5369dfff99316
Rodri <126527883+r0odr1@users.noreply.github.com>
2023-05-03 14:31:40 -0500

    Add files via upload

--- Dia 1/Ejercicio1.js
+++ Dia 1/Ejercicio1.js
@@ -1,2 +1,5 @@
+//Escribe un programa que le pida al usuario ingresar una frase y la imprima en la consola.
+//Recuerda que para pedirle al usuario que escriba una frase debe utilizar el método prompt() y para
+//escribir en la consola debe utilizar el método console.log().
 const frase = prompt("Ingrese una frase");
 console.log(frase);
