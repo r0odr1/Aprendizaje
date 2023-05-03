88d905ebbd76eb2ba92d066dbf0ba64d4955b383
Rodri <126527883+r0odr1@users.noreply.github.com>
2023-05-03 14:33:08 -0500

    Add files via upload

--- Dia 1/Ejercicio2.js
+++ Dia 1/Ejercicio2.js
@@ -1,2 +1,5 @@
+//Escribe un programa que le pregunte al usuario su nombre e imprima "Hola" seguido del nombre
+//y un signo de exclamación.
+//Por ejemplo, si el usuario ingresa "Pedro" el programa debe imprimir en la consola "Hola Pedro!".
 let nombre = prompt('Escriba su nombre: ')
 console.log('hola '+nombre+'!')
