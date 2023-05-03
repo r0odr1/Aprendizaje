a28bd0381b137eef2e0d238b335c07e5331ddaa1
Rodri <126527883+r0odr1@users.noreply.github.com>
2023-05-03 14:34:09 -0500

    Add files via upload

--- Dia 1/Ejercicio3.js
+++ Dia 1/Ejercicio3.js
@@ -1,3 +1,5 @@
+//Escriba un programa que le pida al usuario ingresar un número, luego le pida un segundo número,
+//e imprima en la consola la suma de los dos números que ingresó el usuario.
 let numeroUno = parseInt(prompt('Ingrese un numero: '));
 let numeroDos = parseInt(prompt('Íngrese un segundo numero: '));
 let suma = numeroUno + numeroDos;
