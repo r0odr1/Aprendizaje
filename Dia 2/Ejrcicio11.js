ebbfa9012d71a1c5153b022a1b64fc1df1ab358e
Rodri <126527883+r0odr1@users.noreply.github.com>
2023-05-15 13:57:18 -0500

    Add files via upload

--- Dia 2/Ejrcicio11.js
+++ Dia 2/Ejrcicio11.js
@@ -1,3 +1,11 @@
+/*Escribe un programa que le pida al usuario ingresar un número.
+
+Si el número es múltiplo de 3 debe imprimirse en la consola "bing".
+Si el número es múltiplo de 5 debe imprimirse en la consola "bong".
+Si el número es múltiplo tanto de 3 como de 5 debe imprimirse en la consola "bingbong".
+Si no cumple ninguna de las condiciones anteriores debe imprimir el mismo número.*/
+
+
 let numero = parseInt(prompt("Ingrese un numero: "))
 
 if(numero % 5 === 0 && numero % 3 === 0){
