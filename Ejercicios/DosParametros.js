794e2b615763a6685e6fda8d945f3bf4872c329a
Rodri <126527883+r0odr1@users.noreply.github.com>
2023-05-31 09:58:15 -0500

    Add files via upload

---
+++ Ejercicios/DosParametros.js
@@ -0,0 +1,16 @@
+/* Escribe una funcion llamada twoSum  que reciba dos parametros, un arreglo de numeros
+y un target (numero), si la suma de dos indices del arreglo son iguales al target, retorne el valor
+de esos indices*/
+
+
+nums = [2,7,11,15], target = 9 // [2,7]
+
+function twoSum(arrNum, tar){
+    
+    for(let i = 0; i < arrNum.length; i++){
+        if(arrNum[i] + arrNum[i+1] === tar){
+            return [arrNum[i], arrNum[i+1]]
+        }
+    }
+}
+console.log(twoSum(nums, target))
