eab279fea645ab908f95cfdcf19edeb4046365c7
Rodrigo <rhbon.08@gmail.com>
2023-12-01 18:01:59 -0500

    s

--- 5EjerciciosArray/ejercicio2.js
+++ 5EjerciciosArray/ejercicio2.js
@@ -7,24 +7,24 @@ Las edades se darán en números enteros, por lo que un bebé de 5 meses tendrá
 Devuelve una nueva matriz (una tupla en Python) con [edad más joven, edad más antigua, diferencia entre el más joven y el más viejo edad].
 */
 
-function differenceInAges(age){
-  let numeroMen = age[0]
-  let numeroMax = age[0]
-  if (age.length !== 0){
-      for (let i=0; i < age.length; i++){
-          if (age[i] < numeroMen){
-            numeroMen = age[i]
-          }
-          if (numeroMax < age[i]){
-              numeroMax = age[i]
-          }
-      }
-      diferencia = numeroMax - numeroMen
-      return [numeroMen, numeroMax, diferencia]
-  }
-  
-  return undefined
-}
-console.log(differenceInAges([1, 3, 2, 4])) // 3
-console.log(differenceInAges([10, 9, 8, 7, 6, 5, 4])) // 10
-console.log(differenceInAges([])) // undefined
+// function differenceInAges(age){
+//   let numeroMen = age[0]
+//   let numeroMax = age[0]
+//   if (age.length !== 0){
+//       for (let i=0; i < age.length; i++){
+//           if (age[i] < numeroMen){
+//             numeroMen = age[i]
+//           }
+//           else if (numeroMax < age[i]){
+//               numeroMax = age[i]
+//           }
+//       }
+//       diferencia = numeroMax - numeroMen
+//       return [numeroMen, numeroMax, diferencia]
+//   }
+
+//   return undefined
+// }
+// console.log(differenceInAges([1, 3, 2, 4])) // 3
+// console.log(differenceInAges([10, 9, 8, 7, 6, 5, 4])) // 10
+// console.log(differenceInAges([])) // undefined
