9d9fc838948676bbbbea78e3219048b02d28839e
Rodrigo <rhbon.08@gmail.com>
2023-12-07 13:52:46 -0500

    invertir cadena

---
+++ Ejercicios/invertirCadena.js
@@ -0,0 +1,5 @@
+function invertirCadena(cadena){
+  return [...cadena].reverse().join('');
+}
+
+console.log(invertirCadena("Hola"));
--- Ejercicios/votos.js
+++ Ejercicios/votos.js
@@ -21,14 +21,6 @@
 
 // console.log(contarVotos());
 
-function calcularMedia(arrayNumeros){
-  let sum = 0;
-  for(let i = 0; i < arrayNumeros.length; i++){
-  sum += arrayNumeros[i];
-  }
-  return sum/arrayNumeros.length;
-}
-
 function invertirCadena(cadena) {
   return cadena.split("").reverse().join("");
 }
@@ -38,6 +30,5 @@ function esPalindromo(cadena) {
   return cadena === cadenaReversa;
 }
 
-console.log(calcularMedia([1, 2, 3, 4, 5, 6]));
 console.log(invertirCadena("hola"));
 console.log(esPalindromo("radar"));
