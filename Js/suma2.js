1ab0cfb571d74f6d96c4d4e3ab47476783f94c14
Rodrigo <rhbon.08@gmail.com>
2023-12-14 15:30:38 -0500

    functions

---
+++ Js/suma2.js
@@ -0,0 +1,9 @@
+//1. Crea una función que tome dos números y devuelva su suma.
+function suma (a, b) {
+  let total = a + b
+  return total
+}
+
+console.log(suma(1, 2));
+
+//2. Añade un escuchador de eventos a un botón que dispare la función cuando se haga click en él.
